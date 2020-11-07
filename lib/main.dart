bb2fd0d8e00d3c77fbb688cb72e09e3e94711853
aljaztravnik <travnik.aljaz@gmail.com>
2020-11-07 18:05:11 +0100

    basic homepage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:math';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() => runApp(TravnikDashboard());
 
@@ -33,6 +35,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String time;
+  LocationPermission permission;
   var geolocator = Geolocator();
   Position position;
   var speedInKmh = 0.0;
@@ -49,14 +52,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return DateFormat('MM/dd/yyyy hh:mm:ss').format(dateTime);
   }
 
+  void requestPerms() async
+  {
+    permission = await Geolocator.requestPermission();
+    position = await Geolocator.getLastKnownPosition();
+  }
+
   @override
 
-  void initState() async
+  void initState()
   {
     time = formatDateTime(DateTime.now());
     Timer.periodic(Duration(seconds: 1), (Timer t) => getTime());
-
-    position = await Geolocator.getLastKnownPosition();
     //var options = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
     Geolocator.getPositionStream().listen((position) {
       speedInKmh = position.speed * 3.6; // position.speed is in m/s, so we have to multiply it by 3.6
@@ -65,41 +72,48 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return Scaffold
+    (
+      backgroundColor: Colors.black,
+      appBar: AppBar
+      (
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
+      body: Center
+      (
+        child: Column
+        (
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>
           [
             Row
             (
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>
               [
-                Text("$speedInKmh", style: TextStyle(color: Color(0x000000))),
-                Text("$time", style: TextStyle(color: Color(0x000000))),
+                Text("${speedInKmh.round()} km/h", style: TextStyle(color: Colors.white, fontSize: 50)),
+                Text("$time", style: TextStyle(color: Colors.white, fontSize: 30)),
               ],
             ),
             Padding(padding: EdgeInsets.only(bottom: 10.0)),
             Row
             (
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>
               [
-                FloatingActionButton
+                RaisedButton
                 (
-                  onPressed: () => print("navigacija\n"),
-                  child: Center(child: Text("Navigation", style: TextStyle(color: Color(0xffffff)),)),
-                  backgroundColor: Color(0x404040),
+                  onPressed: () => print("navigation"),
+                  child: Icon(MdiIcons.navigationOutline, color: Colors.black, size: 40),
+                  color: Colors.white,
+                  elevation: 5.0,
                 ),
-                FloatingActionButton
+                RaisedButton
                 (
-                  onPressed: () => print("g-meter\n"),
-                  child: Center(child: Text("G-meter", style: TextStyle(color: Color(0xffffff)),)),
-                  backgroundColor: Color(0x404040),
+                  onPressed: () => print("g-meter\n"), // go to G-meter screen
+                  child: Center(child: Text("G-meter", style: TextStyle(color: Colors.black, fontSize: 30))),
+                  color: Colors.white,
+                  elevation: 5.0,
                 ),
               ],
             ),
